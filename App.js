3a4de0e48f0e8309f06e56af73a3aea4a5ff3091
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-03-31 00:27:09 -0700

    Code the basic layout setup'

--- App.js
+++ App.js
@@ -5,7 +5,16 @@ const App = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-        <Text>Current</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8 </Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>Its sunny</Text>
+        <Text style={styles.message}>Its perfect t-shirt weather</Text>
       </View>
     </SafeAreaView>
   )
@@ -14,11 +23,37 @@ const App = () => {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
+    backgroundColor: 'orange',
   },
   container: {
     backgroundColor: 'pink',
     flex: 1,
     alignItems: 'center',
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  feels: {
+    fontSize: 30,
+    color: 'black',
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20,
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 30,
   },
 })
 
