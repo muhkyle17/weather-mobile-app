c59a9d7b168f5f2edcf83778fb49301c1d819fa0
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-03-30 23:37:06 -0700

    Add some sample code

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React from "react"
-import { View, Text, SafeAreaView } from "react-native"
+import { View, Text, SafeAreaView, StyleSheet } from "react-native"
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>
-          Current Weather oasidjfiosdifosdfisjdiofjiosdiefjisafjesf sfa
-          seasfsief
-        </Text>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text>Current</Text>
       </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  },
+  container: {
+    backgroundColor: "pink",
+    flex: 1,
+    alignItems: "center"
+  }
+})
 
 export default App
