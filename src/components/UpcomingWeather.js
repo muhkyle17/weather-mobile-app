2f869e49ccece7ef1ea9260aeaeb2a2de4bb1844
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-04-02 00:27:45 -0700

    Init  upcoming waether component

---
+++ src/components/UpcomingWeather.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const UpcomingWeather = () => {
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  )
+}
+
+export default UpcomingWeather
