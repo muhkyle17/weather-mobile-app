94f8a43837fd33a219629ff001cf92a5f8e31059
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-04-02 00:06:34 -0700

    Add a sunn icon and add some styling to make the app look nicer

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 const App = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
+        <Feather name="sun" size={100} color="black" />
         <Text style={styles.temp}>6</Text>
         <Text style={styles.feels}>Feels like 5</Text>
         <View style={styles.highLowWrapper}>
@@ -28,6 +30,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
   },
   temp: {
     color: 'black',
@@ -47,6 +50,8 @@ const styles = StyleSheet.create({
   bodyWrapper: {
     justifyContent: 'flex-end',
     alignItems: 'flex-start',
+    paddingLeft: 25,
+    marginBottom: 40,
   },
   description: {
     fontSize: 48,
