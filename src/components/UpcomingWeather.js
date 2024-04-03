d38d16c319d9ae43a4088fdc9ccb8b466fbc3daa
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-04-02 21:47:45 -0700

    Add data to display on the UI

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,5 +1,45 @@
 import React from 'react'
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import { SafeAreaView, Text, StyleSheet, FlatList, View } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+const DATA = [
+  {
+    dt_text: '2023-02-18 12:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: 'Clear',
+      },
+    ],
+  },
+  {
+    dt_text: '2023-02-18 15:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: 'Clouds',
+      },
+    ],
+  },
+  {
+    dt_text: '2023-02-18 18:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: 'Rain',
+      },
+    ],
+  },
+]
 
 const UpcomingWeather = () => {
   return (
