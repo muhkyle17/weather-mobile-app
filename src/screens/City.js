247ab3fc0bd685290e80801472b2f5a0c51747e2
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-04-03 00:40:10 -0700

    Initialize city screen

---
+++ src/screens/City.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text, StyleSheet } from 'react-native'
+
+const City = () => {
+  return (
+    <SafeAreaView>
+      <Text>City</Text>
+    </SafeAreaView>
+  )
+}
+
+export default City
