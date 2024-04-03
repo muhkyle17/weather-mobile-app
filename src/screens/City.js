5405c0021017ef285051472c1a4bd9a0742810d3
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-04-03 00:41:58 -0700

    Display the city screen on the UI'

--- src/screens/City.js
+++ src/screens/City.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { SafeAreaView, Text, StyleSheet } from 'react-native'
 
 const City = () => {
+  const { container } = styles
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={container}>
       <Text>City</Text>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
 
 export default City
