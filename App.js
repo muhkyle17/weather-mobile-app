81ed5de2a66deada368eed49b0b6e1ff611c3c2c
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-04-05 16:31:22 -0700

    Import react navigatin container to app.js

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { View, StyleSheet } from 'react-native'
 import CurrentWeather from './src/screens/CurrentWeather'
 import UpcomingWeather from './src/screens/UpcomingWeather'
 import City from './src/screens/City'
+import { NavigationContainer } from '@react-navigation/native'
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-      {/* <UpcomingWeather /> */}
-      {/* <City /> */}
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <CurrentWeather />
+        {/* <UpcomingWeather /> */}
+        {/* <City /> */}
+      </View>
+    </NavigationContainer>
   )
 }
 
