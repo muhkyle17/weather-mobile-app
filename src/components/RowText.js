99812c10cd62615b9edf21a03fc58f7bbe7544d3
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-04-04 23:01:18 -0700

    Init row text component

---
+++ src/components/RowText.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const RowText = () => {
+  const {} = styles
+
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet({})
+
+export default RowText
