8a41b761652b5df551da0aa54a97ee913541e153
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-04-21 13:58:42 -0700

    Initial template

---
+++ src/demonstration/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+const Counter = () => {
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  )
+}
+
+export default Counter
