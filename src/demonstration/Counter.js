4c7f78452a7ea05e3e5752d7368cdacb3aa40e21
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-04-28 18:14:17 -0700

    Make component for counter state example

--- src/demonstration/Counter.js
+++ src/demonstration/Counter.js
@@ -2,11 +2,27 @@ import React from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 
 const Counter = () => {
+  let count = 0
   return (
-    <View>
-      <Text>Hi</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{`count: ${count}`}</Text>
+      <Button
+        color={'red'}
+        title="Increase the count"
+        onPress={() => {
+          count++
+          console.log(count)
+        }}
+      />
+      <Button
+        color={'green'}
+        title={'Decrease the count'}
+        onPress={() => count--}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({})
 
 export default Counter
