6ee8e15324cf08749529d2a466d3cc7e4fa71cef
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-05-05 23:11:04 -0700

    Add some new count for state change with useeffect example

--- src/demonstration/Counter.js
+++ src/demonstration/Counter.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 
 const Counter = () => {
   const [count, setCount] = useState(0)
+  const [newCount, setNewCount] = useState(0)
+
+  useEffect(() => {
+    console.log(`Count changed`)
+    return () => {
+      console.log('useEffect cleanup')
+    }
+  }, [count])
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{`count: ${count}`}</Text>
@@ -10,14 +19,14 @@ const Counter = () => {
         color={'red'}
         title="Increase the count"
         onPress={() => {
-          setCount(count + 1)
+          setNewCount(count + 1)
         }}
       />
       <Button
         color={'green'}
         title={'Decrease the count'}
         onPress={() => {
-          setCount(count - 1)
+          setNewCount(count - 1)
         }}
       />
     </View>
